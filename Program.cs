5270564a199da3f677ef7d4c62c3f234def4ee3c
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-12 21:48:29 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComparisionProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program ");
+           
+        }
+    }
+}
