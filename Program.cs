585026397b4fd493ec3f070fb44c31e3e411b01c
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-12 23:08:07 +0530

    Calling Method

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program ");
-            CheckEquality.checkEquality();
+            CompareTwoLines.compareTwoLines();
         }
     }
 }
